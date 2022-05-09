46d9bf6f29b3ca91ea553c99f21f5610c93fc0c7
Braedencraig <braeden.craig@gmail.com>
2022-05-09 12:20:27 -0400

    adjust image render

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Character({ name, id, imgSrc, opponent }) {
   return (
     <div data-opponent={opponent} id={id} className={`character ${id === "akuma" && "active"}`}>
-      <img loading="lazy" className={`character__img ${imgSrc.indexOf("question2") !== -1 && "question-img"}`} src={imgSrc} alt={name} />
+      <img loading="lazy" className={`character__img ${imgSrc.indexOf("base64") !== -1 && "question-img"}`} src={imgSrc} alt={name} />
       <p className="character__name">{name}</p>
     </div>
   );
